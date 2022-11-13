43fe888508985888ee0597cce4264146cf756ff4
Nicholas Tutino <tutinonick@gmail.com>
2022-11-12 20:07:41 -0500

    Starting over card structs

--- src/cards/mod.rs
+++ src/cards/mod.rs
@@ -1,36 +1,30 @@
+use std::collections::HashMap;
+
 type Name = String;
 type Dmg = usize;
 type Hp = usize;
 
 pub struct Card {
     name: Name,
-    moves: Vec<Option<Move>>,
+    moves: Moves,
 }
 
-#[derive(Clone)]
-pub struct Move {
-    name: Name,
-    dmg: Dmg,
-}
+type Moves = HashMap<Name, Dmg>;
 
 impl Card {
     pub fn new(name: Name) -> Card {
         Card {
             name,
-            moves: vec![None, None, None, None],
+            moves: HashMap::new(),
         }
     }
-    pub fn add_move(mut self, m: Move) {
-        'assign: for mut mv in self.moves.into_iter() {
-            match mv {
-                Some(_) => continue,
-                None => mv = Some(m.clone()),
-            }
-        }
-    }
+    pub fn add_move(&mut self, name: Name, dmg: Dmg) {
+		
+		self.moves.insert(name, dmg);
+	}
 }
-impl Move {
-    pub fn new(name: Name, dmg: Dmg) -> Move {
-        Move { name, dmg }
-    }
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
 }
